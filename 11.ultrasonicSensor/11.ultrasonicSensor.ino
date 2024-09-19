525f90e41c69a458294e16a58db03afb2798562e
Sebastian Lam <sebastian.lam1@DETNSW>
2024-09-19 10:34:13 +1000

    Update 11.ultrasonicSensor.ino

--- 11.ultrasonicSensor/11.ultrasonicSensor.ino
+++ 11.ultrasonicSensor/11.ultrasonicSensor.ino
@@ -30,8 +30,12 @@ Servo myservo;
 Ultrasonic dingleberry(5);
 
 #define servoPin 7
+
+int distance;
 //unsigned static int potpin = A2;
 
+unsigned long previousMillis;
+unsigned long gateTime = (100);
 void setup() {
   myservo.attach(servoPin);
 
@@ -39,12 +43,21 @@ void setup() {
 }
 
 void loop() {
+unsigned long currentMillis = millis();
+  distance = dingleberry.distanceRead();
+  Serial.println(dingleberry.distanceRead());
 
-Serial.println(dingleberry.distanceRead());
+  if(dingleberry.distanceRead() >=40)
+  {
+    previousMillis = currentMillis;
+  
+     
+    myservo.write(0);
 
-if(dingleberry.distanceRead() >=20){
-myservo.write(0);
-}
-if(dingleberry.distanceRead() <20)
-myservo.write(180);
+     
+  } else 
+  {
+    if(currentMillis - previousMillis >=gateTime)
+    myservo.write(180);
+  }
 }
